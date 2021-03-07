43399fb24ca8bec05a495192de8cd2422b19998e
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-03-07 14:49:11 -0500

    created form for uploading profile image for user

--- blog/pages/views.py
+++ blog/pages/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import redirect, render
+from user.forms import UserProfileForm
+from user.models import UserProfile
 
 
 def index(request):
-    return render(request, 'index.html')
+    form = None
+    current_user = None
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        current_user = UserProfile.objects.get(user_id=user_id)
+
+        if request.method == 'POST':
+            form = UserProfileForm(request.POST, request.FILES)
+            if form.is_valid():
+                current_user.profile_pic = form.cleaned_data['profile_pic']
+                current_user.save()
+            return redirect('/')
+        else:
+            form = UserProfileForm()
+    return render(request, 'index.html', {'form': UserProfileForm, 'user_profile': current_user})
