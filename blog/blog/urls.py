0a979b01c78167f4e2a2ce9883739e118dc6ffdd
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-03-06 20:14:15 -0500

    created homepage, view for user registration

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-    path('user/', include('user.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('user/', include('user.urls')),
+    path('', include('pages.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
