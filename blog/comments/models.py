6926f5dd0f9b13af0e483a54fc48be9e2be2f855
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-04-13 20:40:08 -0400

    created inital model for comment app

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from blog.posts.models import Post
+from blog.user.models import UserProfile
+
+
+class Comment(models.Model):
+    body = models.CharField(max_length=225)
+    creator = models.ForeignKey(
+        UserProfile, related_name='profile', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
