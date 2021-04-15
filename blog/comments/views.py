4a78fe197183e298aa6649844e9563a1c25c07f3
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-04-15 13:00:50 -0400

    added view to comments page

--- blog/comments/views.py
+++ blog/comments/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import HttpResponse, get_object_or_404, render
 from posts.models import Post
 from user.models import UserProfile
 from .forms import CommentForm
+from .models import Comment
+
+
+def comments(pid):
+    return Comment.objects.filter(post_id=pid)
 
 
 def index(request, id):
@@ -9,5 +14,6 @@ def index(request, id):
     user_id = request.user.id
     current_user = UserProfile.objects.get(user_id=user_id)
     form = CommentForm()
+    post_comment = comments(id)
     context = {'post': post, 'form': form}
     return render(request, 'comments/index.html', context)
---
+++ blog/static/js/post.js
@@ -0,0 +1,4 @@
+const body = document.getElementById("id_body");
+
+body.classList.add("form-control");
+body.placeholder = "Nice car !";
